f8fed3f0e76411af8d3e3dfb56d0ae24ede10131
Sébastien Pierre <pierre.sebastien@gmail.com>
2017-02-05 19:23:29 +0100

    improve and add validation on participation form

--- src/app/invitation/invitation.component.ts
+++ src/app/invitation/invitation.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { Restangular } from 'ng2-restangular';
 
 import { HeaderService } from '../shared/services/header.service';
+import { UserService } from '../shared/services/user.service';
 import { INVITATION_LINK } from '../shared/models/link.model';
+import { User } from '../shared/models/user.model';
+import { Participation } from '../shared/models/participation.model';
 
 @Component({
   selector: 'app-invitation',
@@ -12,10 +15,23 @@ import { INVITATION_LINK } from '../shared/models/link.model';
 })
 export class InvitationComponent implements OnInit {
 
-  constructor(private HeaderService: HeaderService, private restangular: Restangular) {
+  currentUser: User;
+  participation: Participation;
+
+  constructor(private HeaderService: HeaderService, private restangular: Restangular, private UserService: UserService) {
   }
 
   ngOnInit() {
-        this.HeaderService.setActiveLink(INVITATION_LINK);
+    this.HeaderService.setActiveLink(INVITATION_LINK);
+    this.UserService.getCurrentUser().subscribe(user => {
+      this.currentUser = user;
+      if (user != null) {
+        this.participation = new Participation(user.id, User.getAddress(user), user.additionnalInfos);
+      }
+    });
+  }
+
+  onsubmit() {
+
   }
 }
--- src/app/shared/models/participation.model.ts
+++ src/app/shared/models/participation.model.ts
@@ -1,13 +1,21 @@
 import { Address } from './address.model';
 
 export class Participation {
-    userId: string;
-    attendings: Attending[];
-    address: Address;
-    additionalInformation: string;
+    constructor(
+        public userId: string,
+        public address: Address,
+        public additionalInformation?: string,
+        public attendings?: Attending[]) {
+
+        if (this.attendings == null) {
+            this.attendings = new Array<Attending>();
+        }
+    }
 }
 
 export class Attending {
-    eventId: string;
-    isAttending: boolean;
+    constructor(
+        public eventId: string,
+        public isAttending?: boolean) {
+    }
 }
