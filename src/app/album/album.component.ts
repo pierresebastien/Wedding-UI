7068305532da9d2d450ac66bdecab4b17af19ca8
Sébastien Pierre <pierre.sebastien@gmail.com>
2017-03-12 14:16:08 +0100

    remove timeline view

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Restangular } from 'ng2-restangular';
-
-import { UserService } from '../shared/services/user.service';
 import { HeaderService } from '../shared/services/header.service';
 import { ALBUM_LINK } from '../shared/models/link.model';
-import { Invitation } from '../shared/models/invitation.model';
 
 @Component({
   selector: 'app-album',
@@ -14,17 +10,10 @@ import { Invitation } from '../shared/models/invitation.model';
 })
 export class AlbumComponent implements OnInit {
 
-  invitations: Invitation[];
-
-  constructor(private HeaderService: HeaderService, private restangular: Restangular, private UserService: UserService) {
+  constructor(private HeaderService: HeaderService) {
     HeaderService.setActiveLink(ALBUM_LINK);
   }
 
   ngOnInit() {
-    this.UserService.getCurrentUser().subscribe(user => {
-      this.UserService.getParticipations(user.id).subscribe(invitations => {
-        this.invitations = invitations;
-      });
-    });
   }
 }
